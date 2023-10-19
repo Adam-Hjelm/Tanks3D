ca7ad1b6b3ac91530f915de7c2e4bd08eed6f78d
Adam Hjelm <adam.hjelm.special@gmail.com>
2023-10-19 17:28:44 +0200

    Added points that track trajectory of shot
    Added points that track trajectory of shot with c++

--- Source/Tanks3D/WheeledVehicleTankPawn.cpp
+++ Source/Tanks3D/WheeledVehicleTankPawn.cpp
@@ -3,25 +3,50 @@
 
 #include "WheeledVehicleTankPawn.h"
 #include "Kismet/GameplayStatics.h"
+//#include<array> We are already including this in the header file that is being included to this file
 
 void AWheeledVehicleTankPawn::SwitchCamera(AActor* CameraOne, AActor* CameraTwo)
 {
-    const float TimeBetweenCameraChanges = 2.0f;
-    const float SmoothBlendTime = 0.75f;
-    //TimeToNextCameraChange -= DeltaTime;
+	const float TimeBetweenCameraChanges = 2.0f;
+	const float SmoothBlendTime = 0.75f;
+	//TimeToNextCameraChange -= DeltaTime;
 
-    APlayerController* OurPlayerController = UGameplayStatics::GetPlayerController(this, 0);
-    if (OurPlayerController)
-    {
-        if ((OurPlayerController->GetViewTarget() != CameraOne) && (CameraOne != nullptr))
-        {
-            // Cut instantly to camera one.
-            OurPlayerController->SetViewTarget(CameraOne);
-        }
-        else if ((OurPlayerController->GetViewTarget() != CameraTwo) && (CameraTwo != nullptr))
-        {
-            // Blend smoothly to camera two.
-            OurPlayerController->SetViewTargetWithBlend(CameraTwo, SmoothBlendTime);
-        }
-    }
+	APlayerController* OurPlayerController = UGameplayStatics::GetPlayerController(this, 0);
+	if (OurPlayerController)
+	{
+		if ((OurPlayerController->GetViewTarget() != CameraOne) && (CameraOne != nullptr))
+		{
+			// Cut instantly to camera one.
+			OurPlayerController->SetViewTarget(CameraOne);
+		}
+		else if ((OurPlayerController->GetViewTarget() != CameraTwo) && (CameraTwo != nullptr))
+		{
+			// Blend smoothly to camera two.
+			OurPlayerController->SetViewTargetWithBlend(CameraTwo, SmoothBlendTime);
+		}
+	}
 }
+
+TArray<FVector> AWheeledVehicleTankPawn::GetPositionsInTrajectory(float timeStep, FVector startVelocity, FVector startPos, float lineSegments, float gravityModifier)
+{
+	TArray<FVector> lineRendPoints; // if you want a fixed size array use TStaticArray
+	lineRendPoints.SetNum(lineSegments);
+
+	lineRendPoints[0] = startPos;
+
+	for (int i = 1; i < lineSegments; i++)
+	{
+		float timeDiff = timeStep * i;
+
+		FVector noGravityPosition = startVelocity * timeDiff;
+		FVector gravityDiff = -0.5f * (9.82 * gravityModifier) * timeDiff * timeDiff * FVector::UpVector;
+		FVector withGravityPosition = startPos + noGravityPosition + gravityDiff;
+		lineRendPoints[i] = withGravityPosition;
+	}
+
+	return lineRendPoints;
+}
+//UE_LOG(LogTemp, Warning, FString::Printf(TEXT("GravityDiff: %f %f %f") gravityDiff.x, gravityDiff.y, gravityDiff.z)); /* Console Debug*/
+
+//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow,  /*Onscreen debug*/
+//	FString::Printf(TEXT("Gravity Diff %f, %f, %f"), gravityDiff.X, gravityDiff.Y, gravityDiff.Z));
--- Source/Tanks3D/WheeledVehicleTankPawn.h
+++ Source/Tanks3D/WheeledVehicleTankPawn.h
@@ -1,21 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "WheeledVehiclePawn.h"
 #include "WheeledVehicleTankPawn.generated.h"
 
 /**
- * 
+ *
  */
+
+ //struct Vector3D
+ //{
+ //	float x, y, z;
+ //};
+
 UCLASS()
 class TANKS3D_API AWheeledVehicleTankPawn : public AWheeledVehiclePawn
 {
 	GENERATED_BODY()
-
 public:
 	UFUNCTION(BlueprintCallable, Category = "Camera")
 	void SwitchCamera(AActor* CameraActor1, AActor* CameraActor2);
-	
+
+	UFUNCTION(BlueprintCallable, Category = "Projectile")
+	TArray<FVector>GetPositionsInTrajectory(float timeStep, FVector startVelocity, FVector startPos, float lineSegments, float gravityModifier);
 };
